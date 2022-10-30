7c0926a0eda63a27e0d3818199724c8f414ff595
cristopher <cristopherortiz457@gmail.com>
2022-10-30 16:33:28 -0600

    Home structure done

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  goToRoom = () => {
+    this.router.navigate(["/", uuidv4()]);
   }
 
 }
