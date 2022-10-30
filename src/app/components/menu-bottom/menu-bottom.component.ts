26b695b8d6955acb28991f643802dc8be4b62b7f
cristopher <cristopherortiz457@gmail.com>
2022-10-30 16:58:49 -0600

    Room canvas done

--- src/app/components/menu-bottom/menu-bottom.component.ts
+++ src/app/components/menu-bottom/menu-bottom.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu-bottom',
   templateUrl: './menu-bottom.component.html',
-  styleUrls: ['./menu-bottom.component.scss']
+  styleUrls: ['./menu-bottom.component.scss'],
 })
 export class MenuBottomComponent implements OnInit {
+  menu: Array<any> = [
+    {
+      name: 'Muted',
+      icon: 'uil uil-microphone',
+    },
+    {
+      name: 'Home',
+      icon: 'uil uil-estate',
+    },
+    {
+      name: 'Share',
+      icon: 'uil uil-share',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
