0e6ba8d95d94c34ad3cd7eb4ed3cd0a47008abac
cristopher <cristopherortiz457@gmail.com>
2022-10-30 18:46:17 -0600

    Room/Video box done

--- src/app/components/video-player/video-player.component.ts
+++ src/app/components/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-video-player',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoPlayerComponent implements OnInit {
 
+  @Input() stream: any  
   constructor() { }
 
   ngOnInit(): void {
--- src/app/pages/room/room.component.ts
+++ src/app/pages/room/room.component.ts
@@ -3,13 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
-  styleUrls: ['./room.component.scss']
+  styleUrls: ['./room.component.scss'],
 })
 export class RoomComponent implements OnInit {
-
-  constructor() { }
+  currentStream: any;
+  listUser: Array<any> = [];
+  constructor() {}
 
   ngOnInit(): void {
+    this.checkMediaDevices();
   }
 
+  checkMediaDevices = () => {
+    if (navigator && navigator.mediaDevices) {
+      navigator.mediaDevices
+        .getUserMedia({
+          audio: false,
+          video: true,
+        })
+        .then((stream) => {
+          this.currentStream = stream;
+          this.addVideoUser(stream);
+        })
+        .catch((e) => console.error('Not permission granted'));
+    } else {
+      console.error('Error Not media devices detected');
+    }
+  };
+
+  addVideoUser = (stream: any) => {
+    this.listUser.push(stream);
+  };
 }
